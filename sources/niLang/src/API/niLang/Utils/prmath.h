d918ff1cf741ef4905d43e89600ee7618f2d758d
Pierre Renaux <pierre@talansoft.com>
2022-06-03 13:55:00 +0800

    niLang/Utils/prmath.h: Added saturate ;

--- sources/niLang/src/API/niLang/Utils/prmath.h
+++ sources/niLang/src/API/niLang/Utils/prmath.h
@@ -21,6 +21,35 @@ ni::sVec3<T> normalize(const ni::sVec3<T>& v) {
 }
 
 template <typename T>
+T saturate(const T& v) {
+  return ni::Clamp(v,(T)0,(T)1);
+}
+
+template <typename T>
+ni::sVec2<T> saturate(const ni::sVec2<T>& v) {
+  ni::sVec2<T> r = v;
+  VecMinimize(r,ni::sVec2f::One(),r);
+  VecMaximize(r,ni::sVec2f::Zero(),r);
+  return r;
+}
+
+template <typename T>
+ni::sVec3<T> saturate(const ni::sVec3<T>& v) {
+  ni::sVec3<T> r = v;
+  VecMinimize(r,ni::sVec3f::One(),r);
+  VecMaximize(r,ni::sVec3f::Zero(),r);
+  return r;
+}
+
+template <typename T>
+ni::sVec4<T> saturate(const ni::sVec4<T>& v) {
+  ni::sVec4<T> r = v;
+  VecMinimize(r,ni::sVec4f::One(),r);
+  VecMaximize(r,ni::sVec4f::Zero(),r);
+  return r;
+}
+
+template <typename T>
 ni::sMatrix<T> translation(const ni::sVec3<T>& p)
 {
   ni::sMatrix<T> m = ni::sMatrix<T>::Identity();

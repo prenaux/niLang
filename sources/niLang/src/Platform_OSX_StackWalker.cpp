3ca17cdd3995c565482ab698c946439ca3748dfc
Pierre Renaux <pierre@talansoft.com>
2022-06-11 23:35:09 +0200

    niLang: Cleaned up the OSX stack walker ;

--- sources/niLang/src/Platform_OSX_StackWalker.cpp
+++ sources/niLang/src/Platform_OSX_StackWalker.cpp
@@ -11,38 +11,36 @@
 namespace ni {
 
 // A C++ function that will produce a stack trace with demangled function and method names.
-cString& Backtrace(cString& out, int skip = 1)
+static cString& Backtrace(cString& aOutput, int skip = 1)
 {
-	void *callstack[128];
-	const int nMaxFrames = sizeof(callstack) / sizeof(callstack[0]);
-	char buf[1024];
-	int nFrames = backtrace(callstack, nMaxFrames);
+  void* callstack[128];
+  const int nMaxFrames = sizeof(callstack) / sizeof(callstack[0]);
+  const int nFrames = backtrace(callstack, nMaxFrames);
 
-	for (int i = skip; i < nFrames; i++) {
-		Dl_info info;
-		if (dladdr(callstack[i], &info)) {
-			char *demangled = NULL;
-			int status;
-			demangled = abi::__cxa_demangle(info.dli_sname, NULL, 0, &status);
-			snprintf(buf, sizeof(buf), "%-3d %p %s + %zd\n",
-               i, (void*)(2 + sizeof(void*) * 2),
-               status == 0 ? demangled : info.dli_sname,
-               (ssize_t)((char *)callstack[i] - (char *)info.dli_saddr));
-			free(demangled);
-		} else {
-			snprintf(buf, sizeof(buf), "%-3d %p\n",
-               i, (void*)(2 + sizeof(void*) * 2));
-		}
-		out += buf;
-	}
-	if (nFrames == nMaxFrames)
-		out += "  [truncated]\n";
-	return out;
+  for (int i = skip; i < nFrames; ++i) {
+    Dl_info info;
+    if (dladdr(callstack[i], &info)) {
+      char *demangled = NULL;
+      int status;
+      demangled = abi::__cxa_demangle(info.dli_sname, NULL, 0, &status);
+      aOutput.CatFormat(
+        "%d: %s + %d\n",
+        i, status == 0 ? demangled : info.dli_sname,
+        (ssize_t)((char *)callstack[i] - (char *)info.dli_saddr));
+      free(demangled);
+    }
+    else {
+      aOutput.CatFormat("%d: unknown\n", i);
+    }
+  }
+  if (nFrames == nMaxFrames) {
+    aOutput << "  [truncated]\n";
+  }
+  return aOutput;
 }
 
 niExportFunc(const cString&) ni_stack_get_current(cString& aOutput, void* apExp) {
-  aOutput = Backtrace(aOutput);
-  return aOutput;
+  return Backtrace(aOutput);
 }
 
 }

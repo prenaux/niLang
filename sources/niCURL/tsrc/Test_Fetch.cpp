7a417b55787e400d8dd498c20283a1967993a011
Pierre Renaux <pierre@bytecollider.com>
2022-10-16 18:06:00 +0800

    niCURL/tsrc: Add FCURLFetch-GetJson and make it the default for embedded targets ;

--- sources/niCURL/tsrc/Test_Fetch.cpp
+++ sources/niCURL/tsrc/Test_Fetch.cpp
@@ -105,4 +105,35 @@ TEST_FIXTURE(FCURLFetch,Post) {
   CHECK_EQUAL(eFalse, request->GetHasFailed());
 }
 
+TEST_FIXTURE(FCURLFetch,GetJson) {
+  Ptr<iMessageQueue> mq = ni::GetOrCreateMessageQueue(ni::ThreadGetCurrentThreadID());
+
+  Nonnull<MyFetchSink> sink = ni::NewNonnull<MyFetchSink>();
+  Nonnull<iFetchRequest> request = _curl->FetchGet(
+    "https://api.coinlore.com/api/ticker/?id=90",
+    sink,
+    NULL).non_null();
+
+  // Dispatch the messages...
+  while (1) {
+    mq->PollAndDispatch();
+    if (request->GetReadyState() == eFetchReadyState_Done)
+      break;
+  }
+
+  cString headers = request->GetReceivedHeaders()->ReadString();
+  niDebugFmt(("... headers: %d bytes, %s",
+              request->GetReceivedHeaders()->GetSize(),
+              headers));
+
+  cString data = request->GetReceivedData()->ReadString();
+  niDebugFmt(("... data: %d bytes, %s",
+              request->GetReceivedData()->GetSize(),
+              data));
+
+  CHECK(data.StartsWith("[{\"id\":\"90\""));
+  CHECK(headers.contains("Content-Type: application/json"));
+  CHECK_EQUAL(eFalse, request->GetHasFailed());
+}
+
 }
--- sources/niCURL/tsrc/Test_Main.cpp
+++ sources/niCURL/tsrc/Test_Main.cpp
@@ -3,8 +3,16 @@
 #include <niLang/Utils/CrashReport.h>
 niCrashReport_DeclareHandler();
 
+#if defined niEmbedded
+#define EMBEDDED_FIXTURE_NAME "FCURLFetch-GetJson"
+#endif
+
 niConsoleMain() {
   ni::cString fixtureName;
+#ifdef EMBEDDED_FIXTURE_NAME
+  fixtureName = EMBEDDED_FIXTURE_NAME;
+#else
   ni::ParseCommandLine(ni::GetCurrentOSProcessCmdLine(),&fixtureName);
+#endif
   return UnitTest::RunAllTests(fixtureName.Chars());
 }

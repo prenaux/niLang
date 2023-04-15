42108665a7af92033d86e13fb80667f704962528
Pierre <pierre@bytecollider.com>
2023-04-15 09:07:29 +0800

    niLang/tsrc/Test_Main.cpp: Print OS environment and fixture name before starting the tests ;

--- sources/niLang/tsrc/Test_Main.cpp
+++ sources/niLang/tsrc/Test_Main.cpp
@@ -12,7 +12,7 @@
 extern bool Test_ChildProcess_Start(int argc, const char** argv, int& ret);
 #endif
 
-int main(int argc, const char** argv) {
+int main(int argc, const char** argv, const char** envp) {
   int r = -1;
 #if !defined niNoProcess
   if (Test_ChildProcess_Start(argc,argv,r)) {
@@ -23,12 +23,24 @@ int main(int argc, const char** argv) {
   {
     niCrashReport_DeclareHandler();
     ni::cString fixtureName;
+
+    printf("I/env:\n");
+    for (const char **env = envp; *env != nullptr; env++) {
+      printf("- %s\n", *env);
+    }
+    fflush(stdout);
+
     ni::ni_log_system_info_once();
+
 #ifdef EMBEDDED_FIXTURE_NAME
     fixtureName = ni::GetProperty("FIXTURE", EMBEDDED_FIXTURE_NAME);
 #else
     ni::ParseCommandLine(ni::GetCurrentOSProcessCmdLine(),&fixtureName);
 #endif
+
+    printf("I/Run all tests with fixtureName '%s'.\n", fixtureName.c_str());
+    fflush(stdout);
+
     return UnitTest::RunAllTests(fixtureName.Chars());
   }
 }

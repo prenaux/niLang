9a68080e16384e7970a41f20b57e46caf5a37a38
Arantir <zx.arantir@gmail.com>
2024-05-30 18:30:03 +0800

    niUI.h: Added missing include "niUI/IJpegWriter.h" for emscripten

--- sources/niUI/src/API/niUI.h
+++ sources/niUI/src/API/niUI.h
@@ -33,6 +33,7 @@
 #include "niUI/IImage.h"
 #include "niUI/ICanvas.h"
 #include "niUI/IGraphics.h"
+#include "niUI/IJpegWriter.h"
 
 #include "niUI/GpuEnum.h"
 #include "niUI/IAccelerationStructure.h"

d0e2dbd9dfea07f5b1fce90c5ed4943b5b490c75
Pierre <pierre@bytecollider.com>
2024-06-09 08:14:14 +0000

    bin/ni-bash-setenv.sh: More robust NI_OS detection, copy ham's logic ;

--- bin/ni-bash-setenv.sh
+++ bin/ni-bash-setenv.sh
@@ -2,25 +2,49 @@
 DIR1=`pwd`
 DIR2="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR1"
-
 if [ "$BUILD" == "" ]; then
    BUILD=$1
 fi
 
-if [ -z $NI_OS ]; then
-    case `uname` in
-        Darwin)
-            export NI_OS=OSX
-            ;;
-        MINGW*_NT*)
-            export NI_OS=NT
-            # Convert all paths for us pls...
-            export MSYS2_ARG_CONV_EXCL=''
-            ;;
-        *)
-            export NI_OS=`uname`
-            ;;
-    esac
+if [[ $OS == Windows* ]]; then
+  export NI_OS=NT
+  export HAM_BIN_LOA=nt-x86
+  if [ -z "$HOME" ]; then
+    HOME=$(unxpath "$USERPROFILE")
+    export HOME
+  fi
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  export NI_OS=OSX
+  UNAME_STR=$(uname)
+  MACHINE_TYPE=$(uname -m)
+  if [ "$MACHINE_TYPE" == "x86_64" ]; then
+    IS_TRANSLATED=$(
+      sysctl -n sysctl.proc_translated 2>/dev/null
+      true
+    )
+    if [ "$IS_TRANSLATED" == "1" ]; then
+      # echo "W/!!! RUNNING UNDER ROSETTA, forcing HAM_BIN_LOA=osx-arm64 !!!"
+      export HAM_BIN_LOA=osx-arm64
+    else
+      export HAM_BIN_LOA=osx-x64
+    fi
+  elif [ "$MACHINE_TYPE" == "arm64" ]; then
+    export HAM_BIN_LOA=osx-arm64
+  else
+    echo "W/Unknown OS '$UNAME_STR' '$MACHINE_TYPE'"
+  fi
+  # export HAM_LOCAL_HOMEBREW="$HAM_HOME/toolsets/_brew/$HAM_BIN_LOA"
+else
+  UNAME_STR=$(/bin/uname)
+  MACHINE_TYPE=$(/bin/uname -m)
+  if [ "$UNAME_STR" == "Linux" ] && [ "$MACHINE_TYPE" == "x86_64" ]; then
+    export NI_OS=LINUX
+    export HAM_BIN_LOA=lin-x64
+    # export HAM_LOCAL_HOMEBREW="$HAM_HOME/toolsets/_brew/$HAM_BIN_LOA"
+  else
+    echo "W/Unknown OS '$UNAME_STR' '$MACHINE_TYPE'"
+    exit 1
+  fi
 fi
 
 BIN_EXT=
@@ -50,15 +74,7 @@ case $NI_OS in
             ;;
         esac
         ;;
-    LINUX86)
-        export BIN_LOA=lin-x86
-        export BIN_EXT=.x86
-        ;;
-    LINUX64)
-        export BIN_LOA=lin-x64
-        export BIN_EXT=.x64
-        ;;
-    Linux)
+    LINUX*)
         export BIN_LOA=lin-x64
         export BIN_EXT=.x64
         ;;

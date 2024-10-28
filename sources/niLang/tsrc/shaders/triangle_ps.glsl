c01974c46d5165461145f3d2d5f3a9f85fc4100b
Pierre <pierre@bytecollider.com>
2024-10-29 01:33:39 +0800

    niLang/tsrc/Test_OSWindow_OSX.mm: Vulkan actually draw the triangle ;

--- sources/niLang/tsrc/shaders/triangle_ps.glsl
+++ sources/niLang/tsrc/shaders/triangle_ps.glsl
@@ -1,4 +1,4 @@
-// glslangValidator -S frag -V triangle_ps.glsl -o triangle_ps.spv && bin2h triangle_ps.spv triangle_ps.spv.h triangle_ps
+// glslangValidator -S frag -V triangle_ps.glsl -o triangle_ps.spv && bin2h triangle_ps.spv triangle_ps.spv.h triangle_ps_spv
 #version 450
 
 layout(location = 0) in vec4 inColor;

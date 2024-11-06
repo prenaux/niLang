96504433c6b61668fc3943623d01ab6758d15aee
Pierre <pierre@bytecollider.com>
2024-11-06 17:37:29 +0800

    niLang/tsrc/shaders: Put only prebuilt vs/ps spv ;

--- sources/niLang/tsrc/shaders/triangle_ps.glsl
+++
@@ -1,9 +0,0 @@
-// glslangValidator -S frag -V triangle_ps.glsl -o triangle_ps.spv && bin2h triangle_ps.spv triangle_ps.spv.h triangle_ps_spv
-#version 450
-
-layout(location = 0) in vec4 inColor;
-layout(location = 0) out vec4 outColor;
-
-void main() {
-  outColor = inColor;
-}

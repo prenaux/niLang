29cab05073cca3ad404b8f6d3e85900ac43b40f2
Pierre <pierre@bytecollider.com>
2024-11-16 23:23:53 +0800

    niUI/Test_Gpu.cpp: Add a matrix rotation to the GpuTexAlpha test ;

--- data/test/gpufunc/TestGpuFuncs.hpp
+++ data/test/gpufunc/TestGpuFuncs.hpp
@@ -10,9 +10,10 @@ struct TestGpuFuncs_TestUniforms {
 
   // Constructor
   TestGpuFuncs_TestUniforms();
-  TestGpuFuncs_TestUniforms(ni::ain<ni::tF32> a_alphaRef, ni::ain<ni::sVec4f> a_materialColor);
+  TestGpuFuncs_TestUniforms(ni::ain<ni::sMatrixf> a_mtxWVP, ni::ain<ni::tF32> a_alphaRef, ni::ain<ni::sVec4f> a_materialColor);
 
   // Variables
+  ni::sMatrixf mtxWVP;
   ni::tF32 alphaRef;
   ni::tF32 padding0;
   ni::tF32 padding1;
@@ -24,13 +25,15 @@ struct TestGpuFuncs_TestUniforms {
 
 // TypeMeth: TestUniforms
 inline TestGpuFuncs_TestUniforms::TestGpuFuncs_TestUniforms() {
+  this->mtxWVP = ni::sMatrixf::Identity();
   this->alphaRef = (ni::tF32)0.1;
   this->padding0 = (ni::tF32)0.0;
   this->padding1 = (ni::tF32)0.0;
   this->padding2 = (ni::tF32)0.0;
   this->materialColor = ni::sVec4f::White();
 }
-inline TestGpuFuncs_TestUniforms::TestGpuFuncs_TestUniforms(ni::ain<ni::tF32> a_alphaRef, ni::ain<ni::sVec4f> a_materialColor) {
+inline TestGpuFuncs_TestUniforms::TestGpuFuncs_TestUniforms(ni::ain<ni::sMatrixf> a_mtxWVP, ni::ain<ni::tF32> a_alphaRef, ni::ain<ni::sVec4f> a_materialColor) {
+  this->mtxWVP = a_mtxWVP;
   this->alphaRef = a_alphaRef;
   this->materialColor = a_materialColor;
 }

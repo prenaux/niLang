92341d26b75bbabf38f7d03846f3b593316f20f1
Pierre <pierre@bytecollider.com>
2024-11-21 13:53:50 +0800

    niUI module: GDRV_Metal.mm, Add support for DrawOpCapture and Remove use of mNumDrawOps; Test_Main.cpp and _build.ham, Add initialization for debug UI and import script module; UnitTest.cpp, Add path for scripts in dataDirs.

--- sources/niUI/tsrc/Test_Main.cpp
+++ sources/niUI/tsrc/Test_Main.cpp
@@ -1,8 +1,10 @@
 #include "stdafx.h"
 #include <niLang/Utils/CrashReport.h>
+#include <niUI/Utils/DebugUI.h>
 niCrashReport_DeclareHandler();
 
 niConsoleMain() {
   niAppLib_SetBuildText();
+  niInitScriptVMForDebugUI();
   return UnitTest::TestAppNativeMainLoop("Test_niUI",argc,argv);
 }

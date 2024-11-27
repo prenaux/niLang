5a8e000ded834beb54aac8e93e53542695ea9272
Pierre <pierre@bytecollider.com>
2024-11-27 15:12:03 +0800

    niLang: Split the graphics API initialization tests in their own fixtures & refactor the Vulkan version so that its not coupled to OSX/Metal/ObjectiveC ;

---
+++ sources/thirdparty/VulkanUtils/niVulkanEnumToString.h
@@ -0,0 +1,132 @@
+#pragma once
+#ifndef __NIVULKANENUMTOSTRING_H_70BEA98A_7024_4D41_99C3_6900A68EE372__
+#define __NIVULKANENUMTOSTRING_H_70BEA98A_7024_4D41_99C3_6900A68EE372__
+//
+// Loosely based on https://github.com/KhronosGroup/Vulkan-Utility-Libraries/blob/main/include/vulkan/vk_enum_string_helper.h
+//
+// This is a different implementation that works with "any" Vulkan SDK and is limited to the enums we care about.
+//
+#include <vulkan/vulkan.h>
+#include <niLang/StringDef.h>
+
+namespace ni_vulkan {
+
+#define VK_CHECK(x,RET) {                                               \
+    VkResult r = (x);                                                   \
+    if (r != VK_SUCCESS) {                                              \
+      niError(niFmt("Vulkan call failed '" #x "': %s", ni_vulkan::VkResultToString(r))); \
+      return RET;                                                       \
+    }                                                                   \
+  }
+
+static inline ni::cString VkResultToString(VkResult aResult) {
+  switch (aResult) {
+    case VK_SUCCESS:
+      return "VK_SUCCESS";
+    case VK_NOT_READY:
+      return "VK_NOT_READY";
+    case VK_TIMEOUT:
+      return "VK_TIMEOUT";
+    case VK_EVENT_SET:
+      return "VK_EVENT_SET";
+    case VK_EVENT_RESET:
+      return "VK_EVENT_RESET";
+    case VK_INCOMPLETE:
+      return "VK_INCOMPLETE";
+    case VK_ERROR_OUT_OF_HOST_MEMORY:
+      return "VK_ERROR_OUT_OF_HOST_MEMORY";
+    case VK_ERROR_OUT_OF_DEVICE_MEMORY:
+      return "VK_ERROR_OUT_OF_DEVICE_MEMORY";
+    case VK_ERROR_INITIALIZATION_FAILED:
+      return "VK_ERROR_INITIALIZATION_FAILED";
+    case VK_ERROR_DEVICE_LOST:
+      return "VK_ERROR_DEVICE_LOST";
+    case VK_ERROR_MEMORY_MAP_FAILED:
+      return "VK_ERROR_MEMORY_MAP_FAILED";
+    case VK_ERROR_LAYER_NOT_PRESENT:
+      return "VK_ERROR_LAYER_NOT_PRESENT";
+    case VK_ERROR_EXTENSION_NOT_PRESENT:
+      return "VK_ERROR_EXTENSION_NOT_PRESENT";
+    case VK_ERROR_FEATURE_NOT_PRESENT:
+      return "VK_ERROR_FEATURE_NOT_PRESENT";
+    case VK_ERROR_INCOMPATIBLE_DRIVER:
+      return "VK_ERROR_INCOMPATIBLE_DRIVER";
+    case VK_ERROR_TOO_MANY_OBJECTS:
+      return "VK_ERROR_TOO_MANY_OBJECTS";
+    case VK_ERROR_FORMAT_NOT_SUPPORTED:
+      return "VK_ERROR_FORMAT_NOT_SUPPORTED";
+    case VK_ERROR_FRAGMENTED_POOL:
+      return "VK_ERROR_FRAGMENTED_POOL";
+    case VK_ERROR_UNKNOWN:
+      return "VK_ERROR_UNKNOWN";
+    case VK_ERROR_OUT_OF_POOL_MEMORY:
+      return "VK_ERROR_OUT_OF_POOL_MEMORY";
+    case VK_ERROR_INVALID_EXTERNAL_HANDLE:
+      return "VK_ERROR_INVALID_EXTERNAL_HANDLE";
+    case VK_ERROR_FRAGMENTATION:
+      return "VK_ERROR_FRAGMENTATION";
+    case VK_ERROR_INVALID_OPAQUE_CAPTURE_ADDRESS:
+      return "VK_ERROR_INVALID_OPAQUE_CAPTURE_ADDRESS";
+    case VK_PIPELINE_COMPILE_REQUIRED:
+      return "VK_PIPELINE_COMPILE_REQUIRED";
+    case VK_ERROR_SURFACE_LOST_KHR:
+      return "VK_ERROR_SURFACE_LOST_KHR";
+    case VK_ERROR_NATIVE_WINDOW_IN_USE_KHR:
+      return "VK_ERROR_NATIVE_WINDOW_IN_USE_KHR";
+    case VK_SUBOPTIMAL_KHR:
+      return "VK_SUBOPTIMAL_KHR";
+    case VK_ERROR_OUT_OF_DATE_KHR:
+      return "VK_ERROR_OUT_OF_DATE_KHR";
+    case VK_ERROR_INCOMPATIBLE_DISPLAY_KHR:
+      return "VK_ERROR_INCOMPATIBLE_DISPLAY_KHR";
+    case VK_ERROR_VALIDATION_FAILED_EXT:
+      return "VK_ERROR_VALIDATION_FAILED_EXT";
+    case VK_ERROR_INVALID_SHADER_NV:
+      return "VK_ERROR_INVALID_SHADER_NV";
+    case VK_ERROR_IMAGE_USAGE_NOT_SUPPORTED_KHR:
+      return "VK_ERROR_IMAGE_USAGE_NOT_SUPPORTED_KHR";
+    case VK_ERROR_VIDEO_PICTURE_LAYOUT_NOT_SUPPORTED_KHR:
+      return "VK_ERROR_VIDEO_PICTURE_LAYOUT_NOT_SUPPORTED_KHR";
+    case VK_ERROR_VIDEO_PROFILE_OPERATION_NOT_SUPPORTED_KHR:
+      return "VK_ERROR_VIDEO_PROFILE_OPERATION_NOT_SUPPORTED_KHR";
+    case VK_ERROR_VIDEO_PROFILE_FORMAT_NOT_SUPPORTED_KHR:
+      return "VK_ERROR_VIDEO_PROFILE_FORMAT_NOT_SUPPORTED_KHR";
+    case VK_ERROR_VIDEO_PROFILE_CODEC_NOT_SUPPORTED_KHR:
+      return "VK_ERROR_VIDEO_PROFILE_CODEC_NOT_SUPPORTED_KHR";
+    case VK_ERROR_VIDEO_STD_VERSION_NOT_SUPPORTED_KHR:
+      return "VK_ERROR_VIDEO_STD_VERSION_NOT_SUPPORTED_KHR";
+    case VK_ERROR_INVALID_DRM_FORMAT_MODIFIER_PLANE_LAYOUT_EXT:
+      return "VK_ERROR_INVALID_DRM_FORMAT_MODIFIER_PLANE_LAYOUT_EXT";
+    case VK_ERROR_NOT_PERMITTED_KHR:
+      return "VK_ERROR_NOT_PERMITTED_KHR";
+    case VK_ERROR_FULL_SCREEN_EXCLUSIVE_MODE_LOST_EXT:
+      return "VK_ERROR_FULL_SCREEN_EXCLUSIVE_MODE_LOST_EXT";
+    case VK_THREAD_IDLE_KHR:
+      return "VK_THREAD_IDLE_KHR";
+    case VK_THREAD_DONE_KHR:
+      return "VK_THREAD_DONE_KHR";
+    case VK_OPERATION_DEFERRED_KHR:
+      return "VK_OPERATION_DEFERRED_KHR";
+    case VK_OPERATION_NOT_DEFERRED_KHR:
+      return "VK_OPERATION_NOT_DEFERRED_KHR";
+    case VK_ERROR_INVALID_VIDEO_STD_PARAMETERS_KHR:
+      return "VK_ERROR_INVALID_VIDEO_STD_PARAMETERS_KHR";
+    case VK_ERROR_COMPRESSION_EXHAUSTED_EXT:
+      return "VK_ERROR_COMPRESSION_EXHAUSTED_EXT";
+    case VK_INCOMPATIBLE_SHADER_BINARY_EXT:
+      return "VK_INCOMPATIBLE_SHADER_BINARY_EXT";
+#ifdef VK_PIPELINE_BINARY_MISSING_KHR
+    case VK_PIPELINE_BINARY_MISSING_KHR:
+      return "VK_PIPELINE_BINARY_MISSING_KHR";
+#endif
+#ifdef VK_ERROR_NOT_ENOUGH_SPACE_KHR
+    case VK_ERROR_NOT_ENOUGH_SPACE_KHR:
+      return "VK_ERROR_NOT_ENOUGH_SPACE_KHR";
+#endif
+    default:
+      return niFmt("<Unhandled VkResult: 0x%x>",(ni::tIntPtr)aResult);
+  }
+}
+
+}
+#endif // __NIVULKANENUMTOSTRING_H_70BEA98A_7024_4D41_99C3_6900A68EE372__
---
+++ sources/thirdparty/VulkanUtils/niVulkanOSXMetal.h
@@ -0,0 +1,64 @@
+#pragma once
+#ifndef __NIVULKANOSXMETAL_H_107EB1C6_87D2_4D40_81D7_6FCC02168685__
+#define __NIVULKANOSXMETAL_H_107EB1C6_87D2_4D40_81D7_6FCC02168685__
+
+#include <vulkan/vulkan.h>
+#include <niLang/Platforms/OSX/osxMetal.h>
+
+#ifdef niVulkanOSXMetal_Implement
+#if !defined(__OBJC__)
+#error "niVulkanOSXMetal.h: niVulkanOSXMetal_Implement must be compiled as Objective-C++, include and define niVulkanOSXMetal_Implement before it in a mm file."
+#endif
+#include <vulkan/vulkan_metal.h>
+#include "niVulkanEnumToString.h"
+#endif
+
+namespace ni {
+
+niExportFunc(tBool) osxVkCreateImageForMetalAPI(
+  iOSXMetalAPI* apMetalAPI,
+  VkDevice avkDevice,
+  const VkAllocationCallbacks* pAllocator,
+  VkImage* pImage)
+#ifdef niVulkanOSXMetal_Implement
+{
+  MTKView* mtkView = (__bridge MTKView*)apMetalAPI->GetMTKView();
+  niCheck(mtkView != nullptr, eFalse);
+
+  MTLRenderPassDescriptor* passDesc = mtkView.currentRenderPassDescriptor;
+  niCheck(passDesc != nullptr, eFalse);
+
+  id<MTLTexture> mtlTexture = passDesc.colorAttachments[0].texture;
+  niCheck(mtlTexture != nullptr, eFalse);
+
+  VkImportMetalTextureInfoEXT importInfo = {
+    .sType = VK_STRUCTURE_TYPE_IMPORT_METAL_TEXTURE_INFO_EXT,
+    .plane = VK_IMAGE_ASPECT_COLOR_BIT,
+    .mtlTexture = mtlTexture
+  };
+
+  VkImageCreateInfo imageInfo = {
+    .sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO,
+    .pNext = &importInfo,
+    .imageType = VK_IMAGE_TYPE_2D,
+    .format = VK_FORMAT_B8G8R8A8_UNORM,
+    .extent = {(tU32)mtlTexture.width, (tU32)mtlTexture.height, 1},
+    .mipLevels = 1,
+    .arrayLayers = 1,
+    .samples = VK_SAMPLE_COUNT_1_BIT,
+    .usage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT
+  };
+
+  VK_CHECK(vkCreateImage(
+    avkDevice,
+    &imageInfo,
+    pAllocator,
+    pImage), eFalse);
+
+  return eTrue;
+}
+#endif
+;
+
+}
+#endif // __NIVULKANOSXMETAL_H_107EB1C6_87D2_4D40_81D7_6FCC02168685__

73631eb06ccc68209e9e2a691db5245806ba8b13
Pierre <pierre@bytecollider.com>
2024-12-12 23:41:28 +0800

    niUI/Test_Gpu.cpp: Add FGpu-TextureCube test with skybox rendering functionality and cube map support.

--- sources/niUI/tsrc_gdrv/Test_Gpu.cpp
+++ sources/niUI/tsrc_gdrv/Test_Gpu.cpp
@@ -902,4 +902,185 @@ struct sFGpu_TextureMipMaps : public sFGpu_Base {
 };
 TEST_CLASS(FGpu,TextureMipMaps);
 
+struct sFGpu_TextureCube : public sFGpu_Base {
+  typedef sVertexP tVertexFmt;
+  NN<iGpuBuffer> _vaBuffer = niDeferredInit(NN<iGpuBuffer>);
+  NN<iGpuBuffer> _iaBuffer = niDeferredInit(NN<iGpuBuffer>);
+  NN<iGpuFunction> _vertexGpuFun = niDeferredInit(NN<iGpuFunction>);
+  NN<iGpuFunction> _pixelGpuFun = niDeferredInit(NN<iGpuFunction>);
+  NN<iGpuPipeline> _pipeline = niDeferredInit(NN<iGpuPipeline>);
+  NN<iTexture> _cubemap = niDeferredInit(NN<iTexture>);
+
+  tBool OnInit(UnitTest::TestResults& testResults_) niOverride {
+    CHECK_RET(sFGpu_Base::OnInit(testResults_),eFalse);
+
+    // Create cube vertices (standard skybox cube)
+    {
+      const tF32 s = 10.0f;
+      const tVertexFmt kCubeVertices[] = {
+        {{-s,  s, -s}}, {{-s, -s, -s}}, {{ s, -s, -s}}, {{ s,  s, -s}},  // front
+        {{ s,  s,  s}}, {{ s, -s,  s}}, {{-s, -s,  s}}, {{-s,  s,  s}},  // back
+        {{-s,  s,  s}}, {{-s, -s,  s}}, {{-s, -s, -s}}, {{-s,  s, -s}},  // left
+        {{ s,  s, -s}}, {{ s, -s, -s}}, {{ s, -s,  s}}, {{ s,  s,  s}},  // right
+        {{-s,  s,  s}}, {{-s,  s, -s}}, {{ s,  s, -s}}, {{ s,  s,  s}},  // top
+        {{-s, -s, -s}}, {{-s, -s,  s}}, {{ s, -s,  s}}, {{ s, -s, -s}}   // bottom
+      };
+
+      _vaBuffer = niCheckNN(
+        _vaBuffer,
+        _driverGpu->CreateGpuBuffer(
+          _H("GpuTextureCube_VA"),
+          sizeof(kCubeVertices),
+          eGpuBufferMemoryMode_Shared,
+          eGpuBufferUsageFlags_Vertex),
+        eFalse);
+
+      tVertexFmt* verts = (tVertexFmt*)_vaBuffer->Lock(0, _vaBuffer->GetSize(), eLock_Discard);
+      niCheck(verts != nullptr, eFalse);
+      memcpy(verts, kCubeVertices, sizeof(kCubeVertices));
+      _vaBuffer->Unlock();
+    }
+
+    // Create indices for the cube
+    {
+      const tU32 kCubeIndices[] = {
+        0,1,2, 2,3,0,       // front
+        4,5,6, 6,7,4,       // back
+        8,9,10, 10,11,8,    // left
+        12,13,14, 14,15,12, // right
+        16,17,18, 18,19,16, // top
+        20,21,22, 22,23,20  // bottom
+      };
+
+      _iaBuffer = niCheckNN(
+        _iaBuffer,
+        _driverGpu->CreateGpuBuffer(
+          _H("GpuTextureCube_IA"),
+          sizeof(kCubeIndices),
+          eGpuBufferMemoryMode_Shared,
+          eGpuBufferUsageFlags_Index),
+        eFalse);
+
+      tU32* inds = (tU32*)_iaBuffer->Lock(0, _iaBuffer->GetSize(), eLock_Discard);
+      niCheck(inds != nullptr, eFalse);
+      memcpy(inds, kCubeIndices, sizeof(kCubeIndices));
+      _iaBuffer->Unlock();
+    }
+
+    // Load cubemap
+#if 1
+    {
+      // Face loading map for left-handed system
+      struct sFaceFile {
+        eBitmapCubeFace face;
+        const achar* filename;
+      } kFaces[] = {
+        {eBitmapCubeFace_PositiveX, "test/tex/skybox_RT.jpg"},
+        {eBitmapCubeFace_NegativeX, "test/tex/skybox_LF.jpg"},
+        {eBitmapCubeFace_PositiveY, "test/tex/skybox_UP.jpg"},
+        {eBitmapCubeFace_NegativeY, "test/tex/skybox_DN.jpg"},
+        {eBitmapCubeFace_NegativeZ, "test/tex/skybox_FR.jpg"},
+        {eBitmapCubeFace_PositiveZ, "test/tex/skybox_BK.jpg"},
+      };
+
+      // Load all faces first
+      Ptr<iBitmap2D> faces[6];
+      for (tU32 i = 0; i < 6; ++i) {
+        faces[i] = (iBitmap2D*)_graphics->LoadBitmapFromRes(_H(kFaces[i].filename),nullptr);
+        niCheck(niIsOK(faces[i]),eFalse);
+      }
+
+      // Create cube with size from first face
+      niLet cubeSize = faces[0]->GetWidth();
+      niLet cubePxf = faces[0]->GetPixelFormat();
+      Ptr<iBitmapCube> bmpCube = _graphics->CreateBitmapCube(
+        cubeSize,"R8G8B8A8");
+      niCheck(niIsOK(bmpCube),eFalse);
+
+      // Blit each face
+      for (tU32 i = 0; i < 6; ++i) {
+        Ptr<iBitmap2D> cubeFace = bmpCube->GetFace(kFaces[i].face);
+        niCheck(niIsOK(cubeFace),eFalse);
+        cubeFace->BlitStretch(faces[i],0,0,0,0,
+                              faces[i]->GetWidth(),faces[i]->GetHeight(),
+                              cubeFace->GetWidth(),cubeFace->GetHeight());
+      }
+
+      _cubemap = AsNN(_graphics->CreateTextureFromBitmap(
+        _H("CubeMap"),
+        bmpCube,
+        eTextureFlags_Default));
+    }
+#else
+    {
+
+      _cubemap = niCheckNN(_cubemap,_graphics->CreateTextureFromRes(
+        // _H("test/tex/glass_cubemap.dds"),
+        _H("test/tex/Beach_cubemap_128.dds"),
+        nullptr,
+        eTextureFlags_Default), eFalse);
+    }
+#endif
+
+    // Create shaders
+    {
+      _vertexGpuFun = niCheckNN(_vertexGpuFun,_driverGpu->CreateGpuFunction(
+        eGpuFunctionType_Vertex,_H("test/gpufunc/skybox_vs.gpufunc.xml")),eFalse);
+      _pixelGpuFun = niCheckNN(_pixelGpuFun,_driverGpu->CreateGpuFunction(
+        eGpuFunctionType_Pixel,_H("test/gpufunc/skybox_ps.gpufunc.xml")),eFalse);
+    }
+
+    // Create pipeline
+    {
+      NN<iGpuPipelineDesc> pipelineDesc = niCheckNN(
+        pipelineDesc, _driverGpu->CreateGpuPipelineDesc(), eFalse);
+      pipelineDesc->SetFVF(tVertexFmt::eFVF);
+      pipelineDesc->SetColorFormat(0,eGpuPixelFormat_BGRA8);
+      pipelineDesc->SetDepthFormat(eGpuPixelFormat_D32);
+      pipelineDesc->SetFunction(eGpuFunctionType_Vertex,_vertexGpuFun);
+      pipelineDesc->SetFunction(eGpuFunctionType_Pixel,_pixelGpuFun);
+      pipelineDesc->SetDepthStencilStates(eCompiledStates_DS_NoDepthTest);
+      pipelineDesc->SetRasterizerStates(eCompiledStates_RS_NoCullingFilled);
+      _pipeline = niCheckNN(_pipeline, _driverGpu->CreateGpuPipeline(
+        _H("GpuTextureCube_Pipeline"),pipelineDesc), eFalse);
+    }
+
+    return eTrue;
+  }
+
+  tBool OnPaint(UnitTest::TestResults& testResults_) niOverride {
+    QPtr<iGraphicsContextGpu> gpuContext = _graphicsContext;
+    NN<iGpuCommandEncoder> cmdEncoder = AsNN(gpuContext->GetCommandEncoder());
+
+    // Setup view matrix that only rotates (no translation for skybox)
+    TestGpuFuncs_TestUniforms u;
+    niLocal rotY = sMatrixf::Identity();
+    niLocal rotX = sMatrixf::Identity();
+    niLocal view = sMatrixf::Identity();
+    niLocal persp = sMatrixf::Identity();
+    MatrixRotationY(rotY,(tF32)_animationTime * 0.3f);
+    MatrixRotationX(rotX,ni::Sin((tF32)_animationTime) * niPi4f);
+    MatrixLookAtLH(view,sVec3f::Zero(),sVec3f::ZAxis(),sVec3f::YAxis());
+    MatrixPerspectiveFovLH(
+      persp,
+      niRadf(110),
+      ni::FDiv(
+        (tF32)_graphicsContext->GetWidth(),
+        (tF32)_graphicsContext->GetHeight()),
+      0.1f,
+      1000.0f);
+    u.mtxWVP = rotY * rotX * view * persp;
+
+    cmdEncoder->StreamUniformBuffer((tPtr)&u,sizeof(u),0);
+    cmdEncoder->SetPipeline(_pipeline);
+    cmdEncoder->SetVertexBuffer(_vaBuffer,0,0);
+    cmdEncoder->SetTexture(_cubemap,0);
+    cmdEncoder->SetSamplerState(eCompiledStates_SS_SmoothClamp,0);
+    cmdEncoder->SetIndexBuffer(_iaBuffer,0,eGpuIndexType_U32);
+    cmdEncoder->DrawIndexed(eGraphicsPrimitiveType_TriangleList,36,0);
+    return eTrue;
+  }
+};
+TEST_CLASS(FGpu,TextureCube);
+
 }

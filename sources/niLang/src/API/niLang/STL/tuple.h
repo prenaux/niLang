c4b4b8ceea1ee98664b4594ae5674336238d0275
Pierre <pierre@talansoft.com>
2025-01-31 00:49:53 +0800

    niLang/STL: Update the tuple implementation and added more type_traits utilities ;

---
+++ sources/niLang/src/API/niLang/STL/tuple.h
@@ -0,0 +1,21 @@
+#pragma once
+#ifndef __STL_TUPLE_H_7E9B1898_ADB5_4849_AEEC_6C643BEE4E44__
+#define __STL_TUPLE_H_7E9B1898_ADB5_4849_AEEC_6C643BEE4E44__
+
+#include "EASTL/tuple.h"
+
+namespace astl {
+
+template <typename... Ts>
+using tuple = eastl::tuple<Ts&...>;
+
+using eastl::apply;
+
+template <size_t... Is>
+using index_sequence = eastl::index_sequence<Is...>;
+
+template <size_t N>
+using make_index_sequence = eastl::make_index_sequence<N>;
+
+}
+#endif // __STL_TUPLE_H_7E9B1898_ADB5_4849_AEEC_6C643BEE4E44__
--- sources/niLang/src/API/niLang/STL/type_traits.h
+++ sources/niLang/src/API/niLang/STL/type_traits.h
@@ -7,6 +7,18 @@
 namespace astl {
 
 using eastl::is_same;
+using eastl::is_same_v;
+using eastl::is_base_of_v;
+using eastl::remove_pointer_t;
+using eastl::remove_cv_t;
+using eastl::remove_cvref_t;
+using eastl::remove_reference_t;
+using eastl::tuple_element_t;
+using eastl::get;
+using eastl::is_void_v;
+
+template<typename T, typename U>
+concept same_as = is_same_v<T, U> && is_same_v<U, T>;
 
 }
 #endif // __STL_TYPE_TRAITS_H_7A014C0E_F03E_0345_80CD_27E2F05EAAFB__

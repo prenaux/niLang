acb885376953e56a39419bd958deb9fe8879b833
Pierre <pierre@talansoft.com>
2025-02-07 22:07:44 +0800

    Cleanup build - 5 - Remove dependency on DX9 SDK and DSHOW SDK ;

--- sources/niUI/src/VideoUtils.h
+++ sources/niUI/src/VideoUtils.h
@@ -3,7 +3,6 @@
 #define __VIDEOUTILS_H_EB54059C_0741_4459_A8A4_43F42399B995__
 
 // Set in the build script
-// #define NI_VIDEO_USE_DSHOW
 // #define NI_VIDEO_USE_FFMPEG
 // #define NI_VIDEO_USE_OGG
 // #define NI_VIDEO_USE_THEORAPLAYER

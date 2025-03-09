1d162fb2368d169e8bbfa9b9e565ed1856bbd41c
pierre <pierre@talansoft.com>
2025-03-10 03:57:42 +0800

    TSGames.sublime-project: Lint & build on macOS ;

--- _scratch_run.sh
+++ _scratch_run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #===== PRELUDE BEGIN ===========
-if [[ -z "$HAM_HOME" ]]; then echo "E/HAM_HOME not set !"; exit 1; fi
+if [[ -z "$HAM_HOME" ]]; then
+  echo "E/HAM_HOME not set !"
+  exit 1
+fi
 # shellcheck disable=SC2034
 SCRIPT_NAME=$(basename "$0")
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
@@ -17,9 +20,9 @@ log_info "Build and run test case"
   export BUILD=da
   export A3=-Dswapinterval=0
 
-  ham Run_Test_niUI FIXTURE=FRayTracer,LitTexturedCube
+  # ham Run_Test_niUI FIXTURE=FRayTracer,LitTexturedCube
   # ham Run_Test_niUI FIXTURE=FRayTracer,Triangle
-  # ham Run_Test_niUI_GDRV FIXTURE=FGpu,Triangle
+  ham Run_Test_niUI_GDRV FIXTURE=FGpu,Triangle
   # ham Run_Test_niUI_GDRV FIXTURE=FRayPipeline,Quad
   # ham Run_Test_niUI_GDRV FIXTURE=FRayPipeline,Triangle
   # ham Run_Test_niUI_GDRV FIXTURE=FRayQuery,IntSphere
